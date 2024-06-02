dcc7a4522c531a691832192f366f25b839099221
hristo2612 <hristoapps@gmail.com>
2024-06-02 17:49:40 +0300

    Initial Commit

---
+++ cb-test/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  cb-test
+//
+//  Created by Hristo on 2.06.24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ cb-test/cb_testApp.swift
@@ -0,0 +1,17 @@
+//
+//  cb_testApp.swift
+//  cb-test
+//
+//  Created by Hristo on 2.06.24.
+//
+
+import SwiftUI
+
+@main
+struct cb_testApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
